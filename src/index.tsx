d73ea3b265eb4a1554f1d9e02b18602e51cbd558
Roni Kantis <roni.kantis@gmail.com>
2017-01-05 01:54:46 +0200

    Cleaned up demo stuff

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import * as React from "react";
 import {App} from "./app/src/App.tsx";
 import {TaxonomyApp} from "./app/src/TaxonomyApp.tsx";
 import {CrimeApp} from "./app/src/crime/CrimeApp.tsx";
-import {Demo1} from "./demo/demo1.tsx"
-import {Demo2} from "./demo/demo2.tsx";
 import {KadaSearch} from "./kadasearch/kadasearch.tsx";
 import {ListApp} from "./app/src/list-app/ListApp.tsx";
 
@@ -18,9 +16,6 @@ ReactDOM.render((
     <Route component={App} path="imdb"/>
     <Route component={TaxonomyApp} path="taxonomy"/>
     <Route component={CrimeApp} path="crime"/>
-
-    <Route component={Demo1} path="demo/demo1"/>
-    <Route component={Demo2} path="demo/demo2"/>
 
     <Route component={ListApp} path="list-app"/>
   </Router>
