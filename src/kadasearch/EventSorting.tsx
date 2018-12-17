169cc7a562a001bc898efb1fa0879e93284d5f46
Matti Hernesniemi <matti.hernesniemi@wunder.io>
2018-12-17 14:54:08 +0200

    Format the query date value and implement an operator for start date

--- src/kadasearch/EventSorting.tsx
+++ src/kadasearch/EventSorting.tsx
@@ -2,9 +2,10 @@ import * as React from "react"
 import * as moment from "moment"
 import { extend } from "lodash"
 
-export const createEventSortQuery = (datefilter) => {
+export const createEventSortQuery = (datefilter:moment.Moment = moment()) => {
+  var formattedQueryDate = datefilter.format('x')
   var dateFilterOn = datefilter ? true : false
-  var now = Date.now()
+  var now = dateFilterOn ? parseInt(formattedQueryDate) : Date.now()
   return {
     "_script": {
       "script": {
