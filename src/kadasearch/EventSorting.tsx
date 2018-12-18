8577350191df58114d1d2df386059b3a83957d1c
Matti Hernesniemi <matti.hernesniemi@wunder.io>
2018-12-18 11:41:38 +0200

    refactor query time logic

--- src/kadasearch/EventSorting.tsx
+++ src/kadasearch/EventSorting.tsx
@@ -6,7 +6,7 @@ export const createEventSortQuery = (datefilter:moment.Moment = moment()) => {
   var formattedQueryDate = datefilter.format('x')
   var dateFilterOn = datefilter ? true : false
   var now = Date.now()
-  var queryNow = dateFilterOn ? parseInt(formattedQueryDate) : Date.now()
+  var queryNow = parseInt(formattedQueryDate)
   return {
     "_script": {
       "script": {
